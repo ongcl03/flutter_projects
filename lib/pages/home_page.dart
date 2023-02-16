57ab22be858a5f309c7d06e0c1298594c33ab258
CL <ongchangle03@gmail.com>
2023-02-16 15:54:51 +0800

    Create Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_projects/models/article_model.dart';
+import 'package:flutter_projects/pages/article_page.dart';
 import 'package:flutter_projects/widgets/bottom_nav_bar.dart';
 import 'package:flutter_projects/widgets/custom_tag.dart';
 
@@ -29,7 +30,10 @@ class HomePage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.zero,
         children: [
-          _NewsOfTheDay(article: article)
+          _NewsOfTheDay(article: article),
+          _BreakingNews(
+            artciles: Article.articles,
+          )
         ],
       ),
     );
@@ -47,6 +51,7 @@ class _NewsOfTheDay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ImageContainer(
+      isContainerTitle: true,
       height: MediaQuery.of(context).size.height * 0.45,
       width: double.infinity,
       imageUrl: article.imageUrl,
@@ -56,17 +61,15 @@ class _NewsOfTheDay extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // News of the day tag
-          CustomTag(
-              backgroundColor: Colors.grey.withAlpha(150),
-              children: [
-                Text(
-                  "News of the day",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(color: Colors.white),
-                ),
-              ]),
+          CustomTag(backgroundColor: Colors.grey.withAlpha(150), children: [
+            Text(
+              "News of the day",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.white),
+            ),
+          ]),
 
           const SizedBox(
             height: 10,
@@ -81,7 +84,8 @@ class _NewsOfTheDay extends StatelessWidget {
 
           TextButton(
               onPressed: () {},
-              style: TextButton.styleFrom(padding: const EdgeInsets.only(left: 3)),
+              style:
+                  TextButton.styleFrom(padding: const EdgeInsets.only(left: 3)),
               child: Row(
                 children: [
                   Text("Learn More",
@@ -98,6 +102,102 @@ class _NewsOfTheDay extends StatelessWidget {
                   )
                 ],
               ))
+        ],
+      ),
+    );
+  }
+}
+
+class _BreakingNews extends StatelessWidget {
+  const _BreakingNews({
+    Key? key,
+    required this.artciles,
+  }) : super(key: key);
+
+  final List<Article> artciles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          // Breaking News and More Text
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Breaking News',
+                style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              Text('More', style: Theme.of(context).textTheme.bodyLarge),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: artciles.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: InkWell(
+                        onTap: (){
+                          Navigator.pushNamed(
+                              context, ArticlePage.routeName,
+                              arguments: artciles[index]
+                          );
+                        },
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ImageContainer(
+                                width: MediaQuery.of(context).size.width * 0.6,
+                                height: 150,
+                                imageUrl: artciles[index].imageUrl,
+                                isContainerTitle: false
+                            ),
+                            const SizedBox(height: 10,),
+                            // Breaking News Title
+                            Padding(
+                              padding: const EdgeInsets.only(left: 5),
+                              child: Text(artciles[index].title,
+                                maxLines: 2,
+                                style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold, height: 1.5, fontSize: 16),
+                              ),
+                            ),
+                            const SizedBox(height: 5,),
+                            // Time posting
+                            Padding(
+                              padding: const EdgeInsets.only(left: 5),
+                              child: Text(
+                                '${DateTime.now().difference(artciles[index].createdAt).inHours} hours ago',
+                                style: Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 13)!,
+                              ),
+                            ),
+                            const SizedBox(height: 5,),
+                            // Author
+                            Padding(
+                              padding: const EdgeInsets.only(left: 5),
+                              child: Text(
+                                'By ${artciles[index].author}',
+                                style: Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 13),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          )
         ],
       ),
     );
--- lib/widgets/image_container.dart
+++ lib/widgets/image_container.dart
@@ -7,6 +7,7 @@ class ImageContainer extends StatelessWidget {
     this.borderRadius = 20,
     required this.width,
     required this.imageUrl,
+    required this.isContainerTitle,
     this.padding,
     this.margin,
     this.child,
@@ -19,6 +20,7 @@ class ImageContainer extends StatelessWidget {
   final EdgeInsets? margin;
   final double borderRadius;
   final Widget? child;
+  final bool isContainerTitle;
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +30,13 @@ class ImageContainer extends StatelessWidget {
       width: width,
       height: height,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: isContainerTitle
+              ? const BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20))
+              : BorderRadius.circular(20),
           image: DecorationImage(
-              image: NetworkImage(imageUrl),
-              fit: BoxFit.cover)
-      ),
+              image: NetworkImage(imageUrl), fit: BoxFit.cover)),
       child: child,
     );
   }
