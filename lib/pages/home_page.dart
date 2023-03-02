06706920f04d870ac54e5a01be15313fb408c465
CL <ongchangle03@gmail.com>
2023-03-02 13:01:24 +0800

    Simple create chatWithGPT function (Future) and working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,8 +12,52 @@ class HomePage extends StatefulWidget {
 
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    chatWithGPT();
+  }
+
+
+  Future chatWithGPT() async{
+    var body = {
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": "Hi"}]
+    };
+    var response = await http.post(
+      Uri.parse("https://api.openai.com/v1/chat/completions"),
+      headers: {
+        // The api key start with sk-...
+        "Authorization" : "Bearer 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",
+        "Content-Type" : "application/json"
+      },
+      body: jsonEncode(body)
+    );
+    var message = jsonDecode(response.body)['choices'][0]['message']['content'];
+    print(message);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
